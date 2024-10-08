5a91aa397038bbe61b9e8df3ec8dc552d7c1b8c0
gabrielestudojava <vanallikaguya@gmail.com>
2024-10-08 08:16:39 -0300

    Update perguntas.js

--- assets/perguntas.js
+++ assets/perguntas.js
@@ -1,1 +1,35 @@
+criaCartao(
+    'Programação 2.0',
+    'O que é phyton?',
+    'O phyton é uma linguagem de progamação'
+)
 
+criaCartao(
+    'Vasco',
+    'Quem fundo o Vasco?',
+    'Mateus Ferreira Biazzeto'
+)
+
+criaCartao(
+    'Batata',
+    'Quem criou a batata frita?',
+    'Mateus Ferreira Biazzeto'
+)
+
+criaCartao(
+    'Futebol',
+    'Quem é conhecido por ser pai do Flamengo?',
+    'O vasco'
+)
+
+criaCartao(
+    'História',
+    'Quem descobriu o Brasil',
+    'Pedro Alvares Cabral'
+)
+
+criaCartao(
+    'Futebol',
+    'Quem foi o rei do futebol?',
+    'Pelé'
+)
