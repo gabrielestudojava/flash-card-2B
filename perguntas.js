c1e5403734339e676f5499b367dd614f6c2a9fbf
gabrielestudojava <vanallikaguya@gmail.com>
2024-10-16 11:48:51 -0300

    ppoj

---
+++ perguntas.js
@@ -0,0 +1,35 @@
+criaCartao(
+    'Programação 2.0',
+    'O que é phyton?',
+    'O phyton é uma linguagem de progamação'
+)
+
+criaCartao(
+    'Vasco',
+    'Quem fundou o Vasco?',
+    'Mc poze do rodo',
+)
+
+criaCartao(
+    'Batata',
+    'Quem plantou batata em contenda?',
+    'Mateus Ferreira Biazzeto'
+)
+
+criaCartao(
+    'Futebol',
+    'Quem é conhecido por ser pai do Flamengo?',
+    'O vasco'
+)
+
+criaCartao(
+    'História',
+    'Quem descobriu o Brasil?',
+    'Kauan Correa'
+)
+
+criaCartao(
+    'Futebol',
+    'Quem construiu o estádio do Vasco?',
+    'Gabriel vanalli'
+)
